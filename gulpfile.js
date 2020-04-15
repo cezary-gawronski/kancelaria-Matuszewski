61878d00f89c7ea2477c11cd9af0c089d48be5f1
cezary-gawronski <idetepowiec@gmail.com>
2020-04-15 03:46:42 +0200

    added mobile media query and watch css into scss folder

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,28 @@ function style() {
     // 1. where is my scss file
     return gulp.src('./scss/**/*.scss')
         // 2. pass that file through sass compiler
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         // 3. where do I save the compiled CSS?
         .pipe(gulp.dest('./css'))
+        // 4. stream changes to all browser
+        .pipe(browserSync.stream());
 }
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./scss/**/*.css', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload)
+}
+
 exports.style = style;
+exports.watch = watch;
 
 
 
